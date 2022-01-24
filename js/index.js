5b9fc8912720c2e2e69ad6b374897d60c063b6d1
AHOUZI HASNAE <92023794+hasnae21@users.noreply.github.com>
2022-01-24 16:53:56 +0100

    u
    u

--- js/index.js
+++ js/index.js
@@ -1,6 +1,18 @@
+var input1 = document.getElementById('zone');
+
 async function get(){
-    const response = await fetch("https://www.weatherapi.com/docs/conditions.json")
-    const data =await response.json()
-    console.log(data)
+    const response = await fetch("http://api.weatherstack.com/current?access_key=92d9a8bc613a702a3273d1f3d81bffd7&query=Taza,Morocco");
+    let data =await response.json()
+    console.log(data);
+//     console.log(data.current.humidity)
 }
 get();
+
+
+// & language = ar makainaxi f api free
+// Forecast
+// & forecast_days = 7
+// & hourly = 1
+// & interval = 3
+
+
