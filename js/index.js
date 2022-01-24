2c455190258e61324728e500a327f1142095c01c
hasnae21 <ahouzi.hasnae.solicode@gmail.com>
2022-01-24 08:34:02 +0100

    d
    d

--- js/index.js
+++ js/index.js
@@ -2,6 +2,5 @@ async function get(){
     const response = await fetch("https://www.weatherapi.com/docs/conditions.json")
     const data =await response.json()
     console.log(data)
-    // document.write(data);
 }
 get();
